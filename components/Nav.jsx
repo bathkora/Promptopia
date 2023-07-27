9d4837a0313bb38117d4bb09b1b50dca86816c35
bathkora <mukesh.bathkora@gmail.com>
2023-07-27 23:53:48 +0530

    Second Commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,16 +6,16 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const {data: session} = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
       setProviders(response);
     };
-    setProviders();
+    setUpProviders();
   }, []);
 
   return (
@@ -31,22 +31,29 @@ const Nav = () => {
         <p className="logo_text">Promptopia</p>
       </Link>
 
+     
+
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
             </Link>
 
-            <button type="button" onClick="signOut" className="outline_btn">
+            <button type="button" 
+            onClick={()=>{
+                    
+                    signOut();
+                }}
+            className="outline_btn">
               Sign Out
             </button>
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
-                width={37}
-                height={37}
+                src={session?.user.image}
+                width={30}
+                height={30}
                 className="rounded-full"
                 alt="profile"
               />
@@ -70,10 +77,10 @@ const Nav = () => {
       </div>
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative ">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex-col">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={30}
               height={30}
               className="rounded-full"
